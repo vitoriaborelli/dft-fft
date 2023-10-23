ed1267e5f370d67bf471c2924fb2745a6ced03c9
vitoriaborelli <113749807+vitoriaborelli@users.noreply.github.com>
2023-10-22 22:54:45 -0300

    feat: create algorithms.py

---
+++ algorithms.py
@@ -0,0 +1,10 @@
+import numpy as np
+def DFT_slow(x):
+    """Compute the discrete Fourier Transform of the 1D array x"""
+    x = np.asarray(x, dtype=float)
+    N = x.shape[0]
+    n = np.arange(N)
+    k = n.reshape((N, 1))
+    M = np.exp(-2j * np.pi * k * n / N)
+    "plota o vetor obrtido após o processamento do sinal"
+    return np.dot(M, x) 
